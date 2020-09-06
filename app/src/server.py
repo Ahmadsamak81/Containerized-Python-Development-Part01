9689a90839175f45540596c4d9e257a6b35ede4d
Ahmadsamak81 <aa.samak81@gmail.com>
2020-09-06 20:07:39 +0300

    update server.py

--- app/src/server.py
+++ app/src/server.py
@@ -3,6 +3,6 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
- return "Hello World!"  
+ return "My names is Ahmad it is Devops Course !!!!"  
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
